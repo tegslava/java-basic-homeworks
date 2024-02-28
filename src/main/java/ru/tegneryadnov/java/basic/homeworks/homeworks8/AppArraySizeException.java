b086ec9d452d299904c057e8cdd0478845639fdc
Vyacheslav Tegneryadnov <tegs@mail.ru>
2024-02-28 11:37:11 +0300

    дз 8 выполнено

--- src/main/java/ru/tegneryadnov/java/basic/homeworks/homeworks8/AppArraySizeException.java
+++ src/main/java/ru/tegneryadnov/java/basic/homeworks/homeworks8/AppArraySizeException.java
@@ -1,20 +1,7 @@
 package ru.tegneryadnov.java.basic.homeworks.homeworks8;
 
 public class AppArraySizeException extends Exception {
-
-    /*    public AppArraySizeException(int size) {
-            super();
-            //super(String.format("Неправильный размер массива. Ожидался двумерный строковый массив [%d][%d]", size, size));
-        }*/
-    private int size;
-
     public AppArraySizeException(int size) {
-        super();
-        this.size = size;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("Ошибка задания размерности массива. Массив должен иметь размерность [%d][%d]", size, size);
+        super(String.format("Ошибка размерности массива. Ожидалась размерность [%d][%d]", size, size));
     }
 }
--- src/main/java/ru/tegneryadnov/java/basic/homeworks/homeworks8/MainApplication.java
+++ src/main/java/ru/tegneryadnov/java/basic/homeworks/homeworks8/MainApplication.java
@@ -1,35 +1,119 @@
 package ru.tegneryadnov.java.basic.homeworks.homeworks8;
 
 public class MainApplication {
-    private static int size = 4;
-
-    public static void main(String[] args) throws AppArraySizeException{
-        String[][] tesArr1 = {{"0", "1", "2", "3", "4"}, {"5", "6", "7", "8"}};
-        String[][] tesArr2 = {{"0", "1", "err", "3"}, {"4", "5", "6", "7"}};
-        String[][] tesArr3 = {{"0", "1", "2", "3"}, {"4", "5", "6", "7"}};
+    public static void main(String[] args) {
+        String[][] arr;
+        int requiredSize = 4;
+        int size;
         try {
-            System.out.println(sumArrElements(tesArr1));
-        } catch (AppArrayDataException e) {
+            size = 4;
+            System.out.printf("На входе метода sumArrElements, массив c цифровыми литералами, размерностью [%d]x[%d]:\n", size, size);
+            arr = get2DArrayDigitalLiterals(size);
+            printArray(arr);
+            System.out.printf("Сумма элементов массива: %d\n", sumArrElements(arr, requiredSize));
+            System.out.printf("На входе метода sumArrElements, массив c литералами, размерностью [%d]x[%d]:\n", size, size);
+            arr = get2DArrayLiterals(size);
+            printArray(arr);
+            System.out.printf("Сумма элементов массива: %d\n", sumArrElements(arr, requiredSize));
+        } catch (AppArraySizeException | AppArrayDataException e) {
             System.out.println(e.getMessage());
         }
 
         try {
-            System.out.println(sumArrElements(tesArr2));
-        } catch (
-                AppArrayDataException e) {
+            size = 5;
+            System.out.printf("На входе метода sumArrElements, массив c цифровыми литералами, размерностью [%d]x[%d]:\n", size, size);
+            arr = get2DArrayDigitalLiterals(size);
+            printArray(arr);
+            System.out.printf("Сумма элементов массива: %d\n", sumArrElements(arr, requiredSize));
+        } catch (AppArraySizeException | AppArrayDataException e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public static long sumArrElements(String[][] sArr) throws AppArraySizeException{
-        if (sArr == null) throw new IllegalArgumentException("На входе ожидался двумерный строковый массив!");
-        if ((sArr.length != 2) || (sArr[0].length != size) || (sArr[1].length != size))
-                throw new AppArraySizeException(size);
+    /*Реализуйте метод, аргументом которого является двумерный строковый массив размером 4х4.
+     Если передан массив другого размера необходимо бросить исключение AppArraySizeException.*/
+
+    /**
+     * Если размерность входящего двумерного массива на соответствует заданному значению,
+     * бросает исключение AppArraySizeException
+     *
+     * @param sArr двумерный строковый массив
+     * @param size заданное значение
+     * @throws AppArraySizeException исключение
+     */
+    public static void checkArrSize(String[][] sArr, int size) throws AppArraySizeException {
+        if (sArr == null) throw new IllegalArgumentException("На входе ожидался двумерный строковый массив ["
+                + size + "]x[" + size + "]!");
+        if (sArr.length != size) throw new AppArraySizeException(size);
+        for (String[] strings : sArr)
+            if (strings.length != size) throw new AppArraySizeException(size);
+    }
+
+    /**
+     * Возвращает строковый квадратный двумерный массив, заполненный цифровыми литералами
+     *
+     * @param size размер массива
+     * @return строковый массив с цифровыми литералами
+     */
+    public static String[][] get2DArrayDigitalLiterals(int size) {
+        String[][] arr = new String[size][size];
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
+                arr[i][j] = String.valueOf(i + j);
+        return arr;
+    }
+
+    /**
+     * Возвращает строковый квадратный двумерный массив, заполненный литералами
+     *
+     * @param size размер массива
+     * @return строковый массив с литералами
+     */
+    public static String[][] get2DArrayLiterals(int size) {
+        String[][] arr = new String[size][size];
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
+                arr[i][j] = String.valueOf(i + j);
+        int randomI = (int) (Math.random() * size);
+        int randomJ = (int) (Math.random() * size);
+        arr[randomI][randomJ] = "A";
+        return arr;
+    }
+
+    /**
+     * Выводит массив на экран
+     *
+     * @param arr массив
+     */
+    public static void printArray(String[][] arr) {
+        for (String[] strings : arr) {
+            for (String string : strings) {
+                System.out.printf("%3s", string);
+            }
+            System.out.println();
+        }
+    }
+
+    /*Метод должен обойти все элементы массива, преобразовать в int и просуммировать.
+    Если в каком-то элементе массива преобразование не удалось (например,
+    в ячейке лежит текст вместо числа), должно быть брошено исключение AppArrayDataException с детализацией,
+    в какой именно ячейке лежат неверные данные.*/
+
+    /**
+     * Возвращает сумму элементов строкового массива, в случае ошибки размерности массива кидает AppArraySizeException,
+     * если невозможно привести типа элемента массива к целому кидает AppArrayDataException
+     *
+     * @param sArr входящий строковый массив
+     * @param size контролируемый размер массива
+     * @return сумма элементов массива
+     */
+    public static long sumArrElements(String[][] sArr, int size) throws AppArraySizeException {
+        checkArrSize(sArr, size);
         long sum = 0;
         for (int i = 0; i < sArr.length; i++) {
             for (int j = 0; j < size; j++) {
                 try {
-                    sum += Integer.valueOf(sArr[i][j]);
+                    sum += Integer.parseInt(sArr[i][j]);
                 } catch (NumberFormatException e) {
                     throw new AppArrayDataException(i, j, sArr[i][j]);
                 }
