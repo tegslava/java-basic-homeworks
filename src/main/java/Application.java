da1962e8de0476c2e717d5a8f1e1a35de309169e
Vyacheslav Tegneryadnov <tegs@mail.ru>
2024-03-15 15:25:07 +0300

    дз 11 переделана, с учетом замечаний. Запуск сделал из Application

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,2 +1,18 @@
+import ru.tegneryadnov.java.basic.homeworks.homework11.PersonDataBase;
+import ru.tegneryadnov.java.basic.homeworks.homework11.SortArray;
+
+import java.util.Arrays;
+
 public class Application {
+    public static void main(String[] args) {
+        PersonDataBase personDataBase = PersonDataBase.getInstance();
+        PersonDataBase.testPersonDataBase(personDataBase);
+        System.out.println();
+        System.out.println("Тест сортировки методом пузырька:");
+        int[] intArray = {1, 20, 149, 0, 3, 567, 34, 2, 15, -999};
+        System.out.printf("Массив на входе:  %s", Arrays.toString(intArray));
+        SortArray.bubbleSort(intArray);
+        System.out.println();
+        System.out.printf("После сортировки: %s", Arrays.toString(intArray));
+    }
 }
--- src/main/java/ru/tegneryadnov/java/basic/homeworks/homework11/MainApplication.java
+++ src/main/java/ru/tegneryadnov/java/basic/homeworks/homework11/MainApplication.java
@@ -4,9 +4,9 @@ import java.util.Arrays;
 
 public class MainApplication {
     public static void main(String[] args) {
-    /*    PersonDataBase personDataBase = PersonDataBase.getInstance();
+        PersonDataBase personDataBase = PersonDataBase.getInstance();
         PersonDataBase.testPersonDataBase(personDataBase);
-        System.out.println();*/
+        System.out.println();
         System.out.println("Тест сортировки методом пузырька:");
         int[] intArray = {1, 20, 149, 0, 3, 567, 34, 2, 15, -999};
         System.out.printf("Массив на входе:  %s", Arrays.toString(intArray));
